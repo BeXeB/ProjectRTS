fee1082885efda663e9a164fb0fae49cfea044cc
Bence Imre Kiss <kissbenceimre99@gmail.com>
2021-05-27 19:59:27 +0200

    unitspawning

---
+++ Assets/Scripts/Buildings/UnitSpawner.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using Mirror;
+
+public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
+{
+    [SerializeField] private GameObject unitPrefab;
+    [SerializeField] private Transform unitSpawnPoint;
+
+
+    #region Server
+
+    [Command]
+    private void CmdSpawnUnit()
+    {
+        GameObject unitInstance = Instantiate(unitPrefab, unitSpawnPoint.position, unitSpawnPoint.rotation);
+        NetworkServer.Spawn(unitInstance, connectionToClient);
+    }
+
+    #endregion
+
+    #region Client
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!hasAuthority)
+        {
+            return;
+        }
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+        CmdSpawnUnit();
+    }
+
+    #endregion
+}
